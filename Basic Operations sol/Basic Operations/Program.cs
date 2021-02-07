d1c181b370eb70fc1871ca90eeb1509126ec3a44
Neehal <19-39435-1@student.aiub.edu>
2021-02-07 19:25:11 +0600

    Lab task

--- Basic Operations sol/Basic Operations/Program.cs
+++ Basic Operations sol/Basic Operations/Program.cs
@@ -11,6 +11,15 @@ namespace Basic_Operations
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+
+            Console.WriteLine(" ");
+
+            int x = 28, y = 8;
+
+            Console.WriteLine("Addition : " + (x + y));
+            Console.WriteLine("Subtraction : " + (x - y));
+            Console.WriteLine("Multiplication : " + (x * y));
+            Console.WriteLine("Division : " + (x / y));
         }
     }
 }
