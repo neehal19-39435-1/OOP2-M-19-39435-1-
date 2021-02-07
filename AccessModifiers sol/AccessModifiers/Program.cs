9a9d5dc3b31c47823db3d606f17ff84d7a3f0c77
Neehal <19-39435-1@student.aiub.edu>
2021-02-07 18:52:54 +0600

    Lab task

---
+++ AccessModifiers sol/AccessModifiers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 28;
+            const int credits = 129;
+            a = 100;
+        }
+    }
+}
---
+++ firstchk sol/firstchk/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstchk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is Neehal !");
+            Console.WriteLine("Today i'm just going to show a simple print operation of numbers and texts ");
+
+            int x = 28, y = 8;
+            Console.WriteLine("Print the values of x & y : {0},{1}", x, y);
+        }
+    }
+}
