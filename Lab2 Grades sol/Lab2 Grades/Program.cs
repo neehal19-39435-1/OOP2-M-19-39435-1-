f9d4bf9521ea134be64b96f4ae56a6cd5fffb2ad
Neehal <19-39435-1@student.aiub.edu>
2021-02-11 17:49:36 +0600

    Tasks

--- Lab2 Grades sol/Lab2 Grades/Program.cs
+++ Lab2 Grades sol/Lab2 Grades/Program.cs
@@ -10,18 +10,61 @@ namespace Lab2_Grades
     {
         static void Main(string[] args)
         {
-            int phy, chem, bio, comp, math, grade;
-            float percentage;
+            double phy, chem, bio, comp, math, total;
+            double percentage;
+            string name, id;
 
-            Console.WriteLine("Enter the marks of all 5 subjects:");
-            int phy = Console.ReadLine();
-            grade = Int32.Parse(phy);
-            int chem = Console.ReadLine();
-            int Bio = Console.ReadLine();
-            int Comp = Console.ReadLine();
-            int Math = Console.ReadLine();
+            Console.WriteLine("Calculating the total percentage and grade of the student:");
+            Console.WriteLine(" ");
 
+            Console.WriteLine("Enter the name of the student:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the ID:");
+            id = Console.ReadLine();
+            Console.WriteLine("Enter the mark of physics:");
+            phy = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the mark of chemistry:");
+            chem = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the marks of biology");
+            bio = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the mark of computer science");
+            comp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enetr the marks of mathematics");
+            math = Convert.ToDouble(Console.ReadLine());
 
+            total = phy + chem + bio + comp + math;
+            percentage = total / 5.0; 
+
+            if (percentage >= 90)
+            {
+                Console.WriteLine("Grade is A+");
+            }
+            else
+                if(percentage <=89 && percentage >=85)
+                {
+                    Console.WriteLine("Grade is A");
+                }
+             else
+                if (percentage <=85 && percentage >=80)
+                {
+                    Console.WriteLine("Grade is B+");
+                }
+             else
+                if(percentage <=79 && percentage >=75)
+                {
+                    Console.WriteLine("Grade is B");
+                }
+             else 
+                if(percentage <=75 && percentage >=50)
+                {
+                    Console.WriteLine("Grade is C+");
+                }
+             else
+                if(percentage < 50)
+                {
+                    Console.WriteLine("Grade is F");
+                }
+            Console.WriteLine("Total percentage is :" + percentage);
         }
     }
 }
