da1ac65277dd5ab65b24f512c39c66f8e8129b81
Neehal <19-39435-1@student.aiub.edu>
2021-02-15 21:38:55 +0600

    Lab 4tasks final

---
+++ Lab 4.5 sol/Lab 4.5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4._5
+{
+    class Program
+    {
+       
+      
+       static void Main(string[] args)
+        {
+            int[] arr = { 1, 1, 4, 5, 6, 6, 7, 9, 9, 9 };
+            int size = arr.Length;
+            int i, j, k;
+
+            Console.WriteLine("Array value: ");
+            for (i = 0; i < size; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            for (i = 0; i < size; i++)
+            {
+              
+
+                for (j = i + 1; j < size; j++)
+                {
+
+                    if (arr[i] == arr[j])
+                    {
+                        for (k = j; k < size - 1; k++)
+                        {
+                            arr[k] = arr[k + 1];
+                        }
+
+                        size--;
+                        j--;
+                    }
+                    }
+                }
+
+                
+            Console.WriteLine("All unique elements : ");
+            for (i = 0; i < size; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+        }
+    }
+
+
